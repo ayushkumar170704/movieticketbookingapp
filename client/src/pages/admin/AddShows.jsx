1ffb00669f75ec50125f422e146fb5d31d4eaca2
AYUSH KUMAR <ayush17kumar07@gmail.com>
2025-07-07 13:25:10 +0530

    frontend part almost complete

--- client/src/pages/admin/AddShows.jsx
+++ client/src/pages/admin/AddShows.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { dummyShowsData } from '../../assets/assets';
+import Loading from '../../components/Loading';
+import Title from '../../components/admin/Title.JSX';
 
 const AddShows = () => {
-  return (
-    <div>AddShows</div>
-  )
+
+  const currency = import.meta.env.VITE_CURRENCY
+  const [nowPlayMovies, setNowPlayingMovies] = useState([]);
+  const [selectedMovie, setSelectedMovie] = useState(null);
+  const [dateTimeSelection, setDateTimeSelection] = useState({});
+  const [dateTimeInput, setDateTimeInput] = useState("")
+  const [showPrice, setShowPrice] = useState("");
+
+
+  const fetchNowPlayingMovies = async()=> {
+    setNowPlayingMovies(dummyShowsData)
+  };
+
+  useEffect(()=>{
+    fetchNowPlayingMovies();
+  },[]);
+
+
+
+
+  return nowPlayMovies.length > 0 ? (
+    <>
+    <Title text1="Add" text2="Shows"/>
+    <p className='mt-10 text-lg font-mdeium'>Now Playing Movies</p>
+    <div className='overflow-x-auto pb-4'>
+      <div className='group flex flex-wrap gap-4 mt-4 w-max'>
+        {setNowPlayingMovies.map((movie)=>(
+          <div key={movie.id} className={`relative max-w-40 cursor-pointer group-hover:novt-hover:opacity-40 hover:-translate-y-1 transition duration-300`}>
+            <div>
+              <img src={mov} alt="" />
+            </div>
+          </div>
+
+
+        ))}
+      </div>
+    </div>
+    
+    
+    </>
+  ) : <Loading/>
 }
 
 export default AddShows
