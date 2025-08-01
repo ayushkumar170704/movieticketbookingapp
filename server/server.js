f3375052865b4d129e4e48a1adce8909ca0f96ec
ayushkumar170704 <ayush17kumar07@gmail.com>
2025-08-01 12:24:03 +0530

    backend

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ import cors from 'cors';
 import 'dotenv/config'
 import connectDB from './configs/db.js';
 import { clerkMiddleware } from '@clerk/express'
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js"
 
 const app = express();
 const port =3000;
@@ -17,6 +19,7 @@ app.use(clerkMiddleware())
 // api routes
 
 app.get('/',(req,res)=> res.send("server is live"))
+app.use("/api/inngest",serve({client:inngest, functions}))
 
-app.listen(port,()=> console.log(`server listening at http://localhost ${port}`));
+app.listen(port,()=> console.log(`server listening at http://localhost:${port}`));
 
