4fd107a0fd1274789c4fbb3fbc569feda30f09ef
Ayush Kumar <ayush17kumar07@gmail.com>
2025-08-14 02:12:23 +0530

    commit

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -2,7 +2,9 @@ import { Inngest } from "inngest";
 import User from "../models/User.js";
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "movie-ticket-booking" });
+export const inngest = new Inngest({ id: "movie-ticket-booking",
+    signingKey: process.env.INNGEST_SIGNING_KEY,
+ });
 
 // Inngest function to save user data to DB
 const syncUserCreation = inngest.createFunction(
